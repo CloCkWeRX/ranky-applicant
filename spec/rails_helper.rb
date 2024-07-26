4950088acf57228a62fa5bea9e1ed62cfd103fa4
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-07-26 12:28:58 +0000

    Add basic rspec/rails_helper

---
+++ spec/rails_helper.rb
@@ -0,0 +1,9 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'spec_helper'
+require File.expand_path('../config/environment', __dir__)
+require 'rspec/rails'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+require 'csv'
+# Add additional requires below this line. Rails is not loaded until this point!
+Rails.application.eager_load!
--- spec/ranking_spec.rb
+++ spec/ranking_spec.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
+require './spec/rails_helper'
+
 describe 'Application Output' do
   describe 'csv rendering' do
     context 'with the exact sample data' do
+      let(:jobs_data) do
+        CSV.read('./spec/test_data/jobs.csv', headers: true)
+      end
+      let(:job_seekers_data) do
+        CSV.read('./spec/test_data/job_seekers.csv', headers: true)
+      end
+      before do
+        jobs_data.each do |row|
+          job = Job.new(name: row[:title])
+          puts row.inspect
+        end
+        # create(:job, name: 'Ruby Developer', job_skills: [])
+        # create(:job, name: '.NET Developer')
+        # create(:job, name: 'C# Developer')
+        # create(:job, name: 'Dev Ops Engineer')
+        # create(:job, name: 'C++ Developer')
+        # create(:job, name: 'Go Developer')
+
+        # create(:job_seeker, name: 'Alice')
+        # create(:job_seeker, name: 'Bob')
+      end
+
       it 'renders a CSV formatted list of applicants' do
         # jobseeker_id, jobseeker_name, job_id, job_title, matching_skill_count, matching_skill_percent
         # 1, Alice, 5, Ruby Developer, 3, 100
