37b64cee037c8e920508aaf2ea0a8de95076b205
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-07-27 02:07:56 +0000

    Add stub of coverage for the data manager

---
+++ spec/data_manager_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require './lib/rails_helper'
+
+describe Ranker::DataManager do
+  describe '#purge!' do
+    it 'removes all existing data'
+  end
+  describe '#load!' do
+    context 'with valid data' do
+      it 'populates the data'
+    end
+    context 'wtih bad data' do
+      it 'raises an IOError'
+    end
+  end
+end
