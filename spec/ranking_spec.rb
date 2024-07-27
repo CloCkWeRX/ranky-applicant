639d4b8765b3740c3f5e2f60ccfac56bc9afc375
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-07-27 02:49:46 +0000

    Assert sort order

--- spec/ranking_spec.rb
+++ spec/ranking_spec.rb
@@ -1,31 +1,10 @@
 # frozen_string_literal: true
 
 require './spec/rails_helper'
+require 'pry'
 
 describe 'Application Output' do
   describe 'csv rendering' do
-    context 'with the primary sample data' do
-      let(:bob) { create(:job_seeker, name: 'Bob', skills: []) }
-      let(:alice) { create(:job_seeker, name: 'Alice') }
-
-      before do
-        # create(:job, name: 'Ruby Developer', job_skills: [])
-        # create(:job, name: '.NET Developer')
-        # create(:job, name: 'C# Developer')
-        # create(:job, name: 'Dev Ops Engineer')
-        # create(:job, name: 'C++ Developer')
-        # create(:job, name: 'Go Developer')
-
-        # jobseeker_id, jobseeker_name, job_id, job_title, matching_skill_count, matching_skill_percent
-        # 1, Alice, 5, Ruby Developer, 3, 100
-        # 1, Alice, 2, .NET Developer, 3, 75
-        # 1, Alice, 7, C# Developer, 3, 75
-        # 1, Alice, 4, Dev Ops Engineer, 4, 50
-        # 2, Bob, 3, C++ Developer, 4, 100
-        # 2, Bob, 1, Go Developer, 3, 75
-      end
-    end
-
     context 'with real world sample data' do
       let(:ranker_service) { Ranker::RankerService.new }
       let(:ranker_data_manager) { Ranker::DataManager.new(jobs_data, job_seekers_data) }
@@ -57,20 +36,32 @@ describe 'Application Output' do
         it 'ranks highest for a Ruby Developer (100%) role' do
           # 1,Alice Seeker,"Ruby, SQL, Problem Solving"
           # 1,Ruby Developer,"Ruby, SQL, Problem Solving"
-          # puts @results[0].inspect
           expect(@results[0][:job_title]).to eq 'Ruby Developer'
           expect(@results[0][:matching_skill_count]).to eq 3
           expect(@results[0][:matching_skill_percent]).to eq 100
         end
 
-        it 'ranks partially for a Fullstack Developer role' do
+        it 'ranks partially for a Backend Developer, then Fullstack Developer role' do
           # 1,Alice Seeker,"Ruby, SQL, Problem Solving"
+          # 3,Backend Developer,"Java, SQL, Node.js, Problem Solving"
           # 4,Fullstack Developer,"JavaScript, HTML/CSS, Node.js, Ruby, SQL, Communication"
-          expect(@results[1][:job_title]).to eq 'Fullstack Developer'
+
+          expect(@results[1][:job_title]).to eq 'Backend Developer'
           expect(@results[1][:matching_skill_count]).to eq 2
-          # expect(@results[1][:matching_skill_percent]).to eq 66 # TODO: Is this how many of the applicant's skills match, or how many of the job's skills match?
+          expect(@results[1][:matching_skill_percent]).to eq 50 # 2 of 4 skills for the job
+
+          expect(@results[2][:job_title]).to eq 'Fullstack Developer'
+          expect(@results[2][:matching_skill_count]).to eq 2
+          expect(@results[2][:matching_skill_percent]).to eq 33 # 2 of 6 skills
+        end
+
+        it 'ranks by job id ascending when equally applicable' do
+          expect(@results[4][:matching_skill_count]).to eq @results[5][:matching_skill_count] # Both have only 1/4 skills
+
+          expect(@results[4][:job_id] < @results[5][:job_id]).to eq true
         end
       end
+
       it 'ranks Ian Jobhunter lowest for Python Developer' do
         # 9,Ian Jobhunter,"JavaScript, React, Self Motivated"
         # 9,Python Developer,"Python, SQL, Problem Solving, Self Motivated"
